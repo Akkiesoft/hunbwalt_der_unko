ae73aa8d50b6dcb87b4d8fd4b9b665e34e3bec07
Toshiaki Asai <toshi.alternative@gmail.com>
2019-07-14 14:57:52 +0900

    fix coding format
    apply rubocop --auto-correct

--- hunbwalt_der_unko.rb
+++ hunbwalt_der_unko.rb
@@ -1,26 +1,26 @@
-# -*- coding: utf-8 -*-
+# frozen_string_literal: true
 
 Plugin.create(:hunbwalt_der_unko) do
-  command(
-          :hunbwalt_der_unko,
+  greets = ['イッヒフンバルト', 'デル', 'ウンコ', 'ハイル！', 'フンデルベン！', 'ミーデルベン！', 'ヘーヒルト', 'ベンデル！', 'フンバルト', 'ヘーデル！', 'ベンダシタイナー！フンデルト', 'モレル', 'ケッツカラデルド！', 'フンベン', 'モルゲン！', 'モーデルワ', 'イッヒ', 'アーデル', 'ゲーベン！', 'ワーデル！']
+
+  command(:hunbwalt_der_unko,
           name: 'イッヒフンバルトデルウンコ',
-          condition: lambda{ |opt| true },
+          condition: ->(_opt) { true },
           visible: true,
-          role: :timeline
-          ) do |opt|
+          role: :timeline) do |opt|
     opt.messages.each do |message|
       Plugin.call(:der_unko, message)
     end
   end
 
-  on_der_unko do | message |
-    greets = ["イッヒフンバルト", "デル", "ウンコ", "ハイル！", "フンデルベン！", "ミーデルベン！", "ヘーヒルト", "ベンデル！", "フンバルト", "ヘーデル！", "ベンダシタイナー！フンデルト", "モレル", "ケッツカラデルド！", "フンベン", "モルゲン！", "モーデルワ", "イッヒ", "アーデル", "ゲーベン！", "ワーデル！"]
-    n_msg = ""
-    msg = "@"+ message.user.idname + " "
-    while TRUE do
+  on_der_unko do |message|
+    n_msg = ''
+    msg = '@' + message.user.idname + ' '
+    loop do
       n_msg = greets.sample
       break if msg.split(//).size + n_msg.split(//).size >= 140
-      msg += n_msg + " "
+
+      msg += n_msg + ' '
     end
     world, = Plugin.filtering(:world_current, nil)
     compose(world, message, body: msg)
